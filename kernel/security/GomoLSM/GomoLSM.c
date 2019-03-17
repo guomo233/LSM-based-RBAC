1605ba1c81bc354e23f5b12ded77d7f069e7339f
guomo <563438460@qq.com>
2019-03-17 21:53:32 +0800

    init commit

---
+++ kernel/security/GomoLSM/GomoLSM.c
@@ -0,0 +1,161 @@
+#include <linux/lsm_hooks.h>
+#include <linux/sysctl.h>
+#include <linux/fs.h>
+#include <linux/sched.h>
+#include <linux/cred.h>
+#include <linux/types.h>
+#include <linux/dcache.h>
+#include <linux/string.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
+
+#define MAX_ROLENAME 20
+#define USER2ROLE_PATH "/etc/GomoLSM/user2role"
+#define ROLES_PATH "/etc/GomoLSM/roles"
+#define PERMISSION_COUNT 2 //inode_create, inode_rename
+
+int get_role (const int uid, char *role)
+{
+	struct file *fout = filp_open (USER2ROLE_PATH, O_RDONLY, 0) ;
+	int _uid ;
+	char uid_buf[sizeof(int)] ;
+	mm_segment_t fs ;
+	int res = 0 ;
+
+	if (!fout || IS_ERR(fout))
+	{
+		printk ("GomoLSM: [get_role] load file error\n") ;
+		return -1 ;
+	}
+	
+	fs = get_fs () ;
+	set_fs (KERNEL_DS) ;
+
+	while ((vfs_read(fout, uid_buf, sizeof(int), &fout->f_pos)) > 0)
+	{
+		memcpy (&_uid, uid_buf, sizeof(int)) ;
+		vfs_read (fout, role, sizeof(char) * (MAX_ROLENAME+1), &fout->f_pos) ;
+		
+		if (uid == _uid)
+		{
+			printk ("GomoLSM: [get_role] uid: %d, role: %s\n", uid, role) ;
+			
+			res = 1 ;
+			break ;
+		}
+	}
+
+	if (res == 0)
+		printk ("GomoLSM: [get_role] uid: %d has no role\n", uid) ;
+
+	set_fs (fs) ;
+	filp_close (fout, NULL) ;
+	
+	return res ;
+}
+
+int role_permission (const char *role, const int op)
+{
+	struct file *fout = filp_open (ROLES_PATH, O_RDONLY, 0) ;
+	mm_segment_t fs ;
+	char _role[MAX_ROLENAME+1] ;
+	char permission_buf[PERMISSION_COUNT * sizeof(int)] ;
+	int permission[PERMISSION_COUNT] ;
+	int _op ;
+	int res = -1 ;
+
+	if (!fout || IS_ERR(fout))
+	{
+		printk ("GomoLSM: [role_permission] load file error\n") ;
+		return -1 ;
+	}
+	
+	fs = get_fs () ;
+	set_fs (KERNEL_DS) ;
+
+	while ((vfs_read(fout, _role, sizeof(char) * (MAX_ROLENAME+1), &fout->f_pos)) > 0)
+	{
+		vfs_read (fout, permission_buf, sizeof(int) * PERMISSION_COUNT, &fout->f_pos) ;
+		memcpy (permission, permission_buf, sizeof(int) * PERMISSION_COUNT) ;
+		
+		if (strcmp(role, _role))
+			continue ;
+
+		for (_op=0; _op<PERMISSION_COUNT; _op++)
+		{
+			if (_op == op && permission[_op] == 1)
+			{
+				printk ("GomoLSM: [role_permission] role: %s has permission\n", role) ;
+					
+				res = 1 ;
+				break ;
+			}
+			else if (_op == op && permission[_op] == 0)
+			{
+				printk ("GomoLSM: [role_permission] role: %s has no permission\n", role) ;
+				res = 0 ;
+				break ;
+			}	
+		}
+	}
+
+	if (res == -1)
+		printk ("GomoLSM: [role_permission] role: %s not exist", role) ;
+
+	set_fs (fs) ;
+	filp_close (fout, NULL) ;
+
+	return res ;
+}
+
+int user_permission (int uid, int op)
+{
+	char role[MAX_ROLENAME+1] ;
+	
+	if (uid <= 999)
+		return 0 ;
+	
+	if (get_role (uid, role) != 1)
+		return 0 ;
+
+	if (role_permission (role, op) == 0)
+		return -1 ;
+
+	return 0 ;
+}
+
+int gmlsm_inode_create (struct inode *dir, struct dentry *dentry, umode_t mode)
+{
+	int uid = current->real_cred->uid.val ;
+	printk ("GomoLSM: call [inode_create] by uid: %d\n", uid) ;
+	
+	return user_permission (uid, 0) ;
+}
+
+int gmlsm_inode_rename (struct inode *old_inode, struct dentry *old_dentry, 
+			struct inode *new_inode, struct dentry *new_dentry)
+{
+	int uid = current->real_cred->uid.val ;
+	printk ("GomoLSM: call [inode_rename] by uid: %d\n", uid) ;
+
+	return user_permission (uid, 1) ;
+}
+
+static struct security_hook_list gmlsm_hooks[] = {
+    LSM_HOOK_INIT(inode_rename,gmlsm_inode_rename),
+    LSM_HOOK_INIT(inode_create,gmlsm_inode_create),
+};
+
+void __init gmlsm_add_hooks(void)
+{
+    pr_info("GomoLSM: LSM LOADED.\n");
+    security_add_hooks(gmlsm_hooks, ARRAY_SIZE(gmlsm_hooks));
+}
+
+static __init int gmlsm_init(void){
+    gmlsm_add_hooks();
+    return 0;
+}
+
+security_initcall(gmlsm_init);
---
+++ role_manager.c
@@ -0,0 +1,513 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define USER2ROLE_PATH "/etc/GomoLSM/user2role"
+#define ROLES_PATH "/etc/GomoLSM/roles"
+#define MAX_ROLENAME 20
+#define PERMISSION_COUNT 2 //inode_create, inode_rename
+
+int write_user2role (int uid, const char *role)
+{
+	FILE *fp = fopen (USER2ROLE_PATH, "ab") ;
+	
+	if (!fp)
+	{
+		printf ("file open failed\n") ;
+		return -1 ;
+	}
+	
+	fwrite (&uid, sizeof(int), 1, fp) ;
+	fwrite (role, sizeof(char), MAX_ROLENAME+1, fp) ;
+	
+	fclose (fp) ;
+	return 1 ;
+}
+
+int user_exists (int uid)
+{
+	int _uid ;
+	FILE *fp = fopen (USER2ROLE_PATH, "rb") ;
+	
+	if (!fp)
+	{
+//		printf ("file open failed\n") ;
+		return -1 ;
+	}
+	
+	while (fread(&_uid, sizeof(int), 1, fp))
+	{
+		if (_uid == uid)
+		{
+			fclose (fp) ;
+			return 1 ;
+		}
+			
+		fseek (fp, sizeof(char) * (MAX_ROLENAME + 1), SEEK_CUR) ;
+	}
+	
+	fclose (fp) ;
+	return 0 ;
+}
+
+int add_user2role (int uid, const char *role)
+{
+	int role_len = strlen (role) ;
+	int _uid ;
+	int i ;
+	char _role[MAX_ROLENAME+1] ;
+	
+	if (role_len > MAX_ROLENAME)
+	{
+		printf ("Role name too long\n") ;
+		return 0 ;
+	}
+	if (user_exists(uid) == 1)
+	{
+		printf ("The role of user already exists\n") ;
+		return 0 ;
+	}
+	
+	for (i=0; i<MAX_ROLENAME; i++)
+	{
+		if (i < role_len)
+				_role[i] = role[i] ;
+		else
+				_role[i] = '\0' ;
+	}
+	
+	return write_user2role(uid, _role) ;
+}
+
+int write_role (const char *role, const int *permission)
+{
+	FILE *fp = fopen (ROLES_PATH, "ab") ;
+		
+	if (!fp)
+	{
+		printf ("file open failed\n") ;
+		return -1 ;
+	}
+	
+	fwrite (role, sizeof(char), MAX_ROLENAME+1, fp) ;
+	fwrite (permission, sizeof(int), PERMISSION_COUNT, fp) ;
+	
+	fclose (fp) ;
+	return 1 ;
+}
+
+int role_exists (const char *role)
+{
+	char _role[MAX_ROLENAME+1] ;
+	FILE *fp = fopen (ROLES_PATH, "rb") ;
+	
+	if (!fp)
+	{
+//		printf ("file open failed\n") ;
+		return -1 ;
+	}
+	
+	while (fread(_role, sizeof(char), MAX_ROLENAME+1, fp))
+	{
+		if (!strcmp (_role, role))
+		{
+			fclose (fp) ;
+			return 1 ;
+		}
+			
+		fseek (fp, sizeof(int) * PERMISSION_COUNT, SEEK_CUR) ;
+	}
+	
+	fclose (fp) ;
+	return 0 ;
+}
+
+int add_role (const char *role, const int *permission)
+{
+	int role_len = strlen (role) ;
+	char _role[MAX_ROLENAME+1] ;
+	int i ;
+	
+	if (role_len > MAX_ROLENAME)
+	{
+		printf ("Role name too long\n") ;
+		return 0 ;
+	}
+	if (sizeof(permission) != PERMISSION_COUNT * sizeof(int))
+	{
+		printf ("invalid [permission] setting\n") ;
+		return 0 ;
+	}
+	if (role_exists(role) == 1)
+	{
+		printf ("Role already exists\n") ;
+		return 0 ;
+	}
+			
+	for (i=0; i<MAX_ROLENAME; i++)
+	{
+		if (i < role_len)
+				_role[i] = role[i] ;
+		else
+				_role[i] = '\0' ;
+	}
+	
+	return write_role (_role, permission) ;
+}
+
+int del_user2role (int uid)
+{
+	FILE *fp = fopen (USER2ROLE_PATH, "rb") ;
+	int uids[100] ;
+	int count = 0 ;
+	int i ;
+	
+	char roles[100][MAX_ROLENAME+1] ;
+	
+	if (!fp)
+	{
+		printf ("open file failed\n") ;
+		return -1 ;
+	}
+	
+	while (fread (&uids[count], sizeof(int), 1, fp))
+	{
+		fread (roles[count], sizeof(char), MAX_ROLENAME+1, fp) ;
+
+		if (uids[count] != uid)
+			count++ ;
+	}
+	
+	fclose (fp) ;
+	fp = fopen (USER2ROLE_PATH, "w") ;
+	for (i=0; i<count; i++)
+	{
+		fwrite (&uids[i], sizeof(int), 1, fp) ;
+		fwrite (roles[i], sizeof(char), MAX_ROLENAME+1, fp) ;
+	}
+	
+	fclose (fp) ;
+	return 1 ;
+}
+
+int del_role (const char *role)
+{
+	FILE *fp = fopen (ROLES_PATH, "rb") ;
+	char roles[100][MAX_ROLENAME+1] ;
+	int permissions[100][PERMISSION_COUNT] ;
+	int count = 0 ;
+	int i ;
+	
+	if (!fp)
+	{
+		printf ("open file failed\n") ;
+		return -1 ;
+	}
+	
+	while (fread (roles[count], sizeof(char), MAX_ROLENAME+1, fp))
+	{
+		fread (permissions[count], sizeof(int), PERMISSION_COUNT, fp) ;
+
+		if (strcmp(roles[count], role))
+			count++ ;
+	}
+	
+	fclose (fp) ;
+	fp = fopen (ROLES_PATH, "w") ;
+	for (i=0; i<count; i++)
+	{
+		fwrite (roles[i], sizeof(char), MAX_ROLENAME+1, fp) ;
+		fwrite (permissions[i], sizeof(int), PERMISSION_COUNT, fp) ;
+	}
+	
+	fclose (fp) ;
+	return 1 ;
+}
+
+int change_user2role (int uid, const char *role)
+{
+	int res1 = del_user2role (uid) ;
+	int res2 = add_user2role (uid, role) ;
+	
+	return res1 * res2 ;
+}
+
+int change_role (const char *role, const int *permission)
+{
+	int res1 = del_role (role) ;
+	int res2 = add_role (role, permission) ;
+	
+	return res1 * res2 ;
+}
+
+int show_user2role ()
+{
+	FILE *fp = fopen (USER2ROLE_PATH, "rb") ;
+	int uid ;
+	char role[MAX_ROLENAME+1] ;
+	
+	if (!fp)
+	{
+		printf ("No items now\n") ;
+		return -1 ;
+	}
+	
+	while (fread (&uid, sizeof(int), 1, fp))
+	{
+		fread (role, sizeof(char), MAX_ROLENAME+1, fp) ;
+		printf ("uid %d : %s\n", uid, role) ;
+	}
+	
+	fclose (fp) ;
+	return 1 ;
+}
+
+int show_roles ()
+{
+	FILE *fp = fopen (ROLES_PATH, "rb") ;
+	int permission[PERMISSION_COUNT] ;
+	char role[MAX_ROLENAME+1] ;
+	
+	if (!fp)
+	{
+		printf ("No items now\n") ;
+		return -1 ;
+	}
+	
+	while (fread (role, sizeof(char), MAX_ROLENAME+1, fp))
+	{
+		fread (permission, sizeof(int), PERMISSION_COUNT, fp) ;
+		printf ("%s permission: inode_create[%d], inode_rename[%d]\n", role, permission[0], permission[1]) ;
+	}
+	
+	fclose (fp) ;
+	return 1 ;
+}
+
+int _pow10 (int p)
+{
+	int res = 1 ;
+	int i ;
+	
+	for (i=1; i<=p; i++)
+		res *= 10 ;
+		
+	return res ;
+}
+
+int s2i (const char *s)
+{
+	int len = strlen (s) ;
+	int i ;
+	int res = 0 ;
+	int temp ;
+	
+	for (i=0; i<len; i++)
+	{
+		temp = s[i] - '0' ;
+		if (temp > 9 || temp < 0)
+		{
+			printf ("the string can not convert to number\n") ;
+			return -1 ;
+		}
+		
+		res += temp * _pow10(len - i - 1) ;
+	}
+	
+	return res ;
+}
+
+int main(int argc, char *argv[])
+{
+	int i ;
+	int permission_count ;
+	int permission[PERMISSION_COUNT] ;
+	int uid ;
+	
+	if (argc == 1)
+	{
+		printf ("Error: missing arguments\n") ;
+		return 0 ;
+	}
+		
+	if (!strcmp(argv[1], "-s"))
+	{
+		if (argc != 3)
+		{
+			printf ("Error: invalid number of arguments after -s\n") ;
+			return 0 ;
+		}
+			
+		if (!strcmp(argv[2], "user2role"))
+		{
+			show_user2role() ;
+			return 0 ;
+		}
+			
+		if (!strcmp(argv[2], "roles"))
+		{
+			show_roles() ;
+			return 0 ;
+		}
+			
+		printf ("Error: unkonw argument after -s\n") ;
+		return 0 ;
+	}
+	
+	if (!strcmp(argv[1], "-ra"))
+	{
+		if (argc != 4)
+		{
+			printf ("Error: invalid number of arguments after -ra\n") ;
+			return 0 ;
+		}
+			
+		permission_count = strlen (argv[3]) ;
+		if (permission_count != PERMISSION_COUNT)
+		{
+			printf ("Error: invalid [permission] setting after -ra\n") ;
+			return 0 ;
+		}
+
+		for (i=0; i<permission_count; i++)
+		{
+			permission[i] = argv[3][i] - '0' ;
+			
+			if (permission[i] != 0 && permission[i] != 1)
+			{
+				printf ("Error: invalid [permission] setting after -ra\n") ;
+				return 0 ;
+			}
+		}
+		
+		if (add_role(argv[2], permission) == 1)
+			printf ("Role added successfully\n") ;
+		else
+			printf ("Role add failed\n") ;
+		
+		return 0 ;
+	}
+	
+	if (!strcmp(argv[1], "-ua"))
+	{
+		if (argc != 4)
+		{
+			printf ("Error: invalid number of arguments after -ua\n") ;
+			return 0 ;
+		}
+		
+		uid = s2i (argv[2]) ;
+		if (uid == -1)
+		{
+			printf ("Error: invalid [uid] setting after -ua\n") ;
+			return 0 ;
+		}
+		
+		if (add_user2role(uid, argv[3]) == 1)
+			printf ("The role of user added successfully\n") ;
+		else
+			printf ("The role of user add failed\n") ;
+		
+		return 0 ;
+	}
+	
+	if (!strcmp(argv[1], "-rd"))
+	{
+		if (argc != 3)
+		{
+			printf ("Error: invalid number of arguments after -rd\n") ;
+			return 0 ;
+		}
+		
+		if (del_role(argv[2]) == 1)
+			printf ("Role deleted successfully\n") ;
+		else
+			printf ("Role delete failed\n") ;
+		
+		return 0 ;
+	}
+	
+	if (!strcmp(argv[1], "-ud"))
+	{
+		if (argc != 3)
+		{
+			printf ("Error: invalid number of arguments after -ud\n") ;
+			return 0 ;
+		}
+		
+		uid = s2i (argv[2]) ;
+		if (uid == -1)
+		{
+			printf ("Error: invalid [uid] setting after -ud\n") ;
+			return 0 ;
+		}
+		
+		if (del_user2role(uid) == 1)
+			printf ("The role of user deleted successfully\n") ;
+		else
+			printf ("The role of user delete failed\n") ;
+		
+		return 0 ;
+	}
+	
+	if (!strcmp(argv[1], "-rc"))
+	{
+		if (argc != 4)
+		{
+			printf ("Error: invalid number of arguments after -rc\n") ;
+			return 0 ;
+		}
+			
+		permission_count = strlen (argv[3]) ;
+		if (permission_count != PERMISSION_COUNT)
+		{
+			printf ("Error: invalid [permission] setting after -rc\n") ;
+			return 0 ;
+		}
+
+		for (i=0; i<permission_count; i++)
+		{
+			permission[i] = argv[3][i] - '0' ;
+			
+			if (permission[i] != 0 && permission[i] != 1)
+			{
+				printf ("Error: invalid [permission] setting after -rc\n") ;
+				return 0 ;
+			}
+		}
+		
+		if (change_role(argv[2], permission) == 1)
+			printf ("Role changed successfully\n") ;
+		else
+			printf ("Role change failed\n") ;
+		
+		return 0 ;
+	}
+	
+	if (!strcmp(argv[1], "-uc"))
+	{
+		if (argc != 4)
+		{
+			printf ("Error: invalid number of arguments after -uc\n") ;
+			return 0 ;
+		}
+		
+		uid = s2i (argv[2]) ;
+		if (uid == -1)
+		{
+			printf ("Error: invalid [uid] setting after -uc\n") ;
+			return 0 ;
+		}
+
+		if (change_user2role(uid, argv[3]) == 1)
+			printf ("The role of user changed successfully\n") ;
+		else
+			printf ("The role of user change failed\n") ;
+		
+		return 0 ;
+	}
+
+	printf ("Error: invalid argument option\n") ;
+	return 0 ;
+}
